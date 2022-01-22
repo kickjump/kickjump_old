de99796fa84886a315991191734597f1f215c622
Ifiok Jr <ifiokotung@gmail.com>
2022-01-22 17:10:53 +0000

    feat: first commit 🎉🥳

---
+++ .monots/symlink/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['monots'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['monots/full'],
+      parserOptions: {
+        project: ['./.monots/tsconfig.lint.json'],
+      },
+    },
+    {
+      files: ['*.tsx'],
+      extends: ['monots/react'],
+    },
+    {
+      files: ['**/vite.config.{js,ts}'],
+      rules: { 'import/no-default-export': 'off' },
+    },
+  ],
+};
