c13b690c97dccb957f92ae28b089d6bf1c0c3dc3
Ifiok Jr <ifiokotung@gmail.com>
2022-01-22 19:13:02 +0000

    feat: create remix site from template

---
+++ website/app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>KickJump</h1>
+      <p>Welcome to KickJump.</p>
+    </div>
+  );
+}
