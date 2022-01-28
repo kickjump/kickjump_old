a612dbeedd441139743eca77f6628c55fac4c9ec
Ifiok Jr <ifiokotung@gmail.com>
2022-01-28 01:04:14 +0000

    build: resolve typecheck issue and tsconfigs

--- packages/kickjump__db/src/prisma.ts
+++ packages/kickjump__db/src/prisma.ts
@@ -1,11 +1,6 @@
-import Module = require('./generated'); // Required with the current build tool.
-// import { PrismaClient } from './generated';
-// import * as Module from './generated';
+// Required with the current build tool.
+import Module = require('./generated');
 
 export * from './generated';
 
-/**
- * @internal
- */
-// export const prisma = new PrismaClient();
 export const prisma = new Module.PrismaClient();
